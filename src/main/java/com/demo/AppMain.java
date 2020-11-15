ef30bc8dd590dff01294281dde9e7c638d905e07
shekoofehp <shekoofeh.poorizadeh@gmail.com>
2020-11-15 10:26:41 -0600

    first commit

---
+++ src/main/java/com/demo/AppMain.java
@@ -0,0 +1,9 @@
+package com.demo;
+
+public class AppMain {
+    public static void main(String[] args){
+        //first commit
+        System.out.println("This is a maven project, we are going to deploy it to an embedded Tom cat.");
+
+    }
+}
